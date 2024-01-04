f77733f28622c009455c34ed43d7f48b55bd4b17
lurgi <wahoo9040@naver.com>
2024-01-04 09:31:26 +0900

    feat(*): React Query Dev Tools 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import Debounce from "./routes/Debounce";
 import VirtualizedList from "./routes/VirtualizedList";
 
 import Header from "./components/Header";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   return (
     <BrowserRouter>
       <QueryClientProvider client={new QueryClient()}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <Header />
         <main className="py-2 flex flex-col items-center">
           <Routes>
